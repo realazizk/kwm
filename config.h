0f31f570a2508c5a30659d765872e72311e3053d
mohamed-aziz <medazizknani@gmail.com>
2018-03-22 21:25:09 +0100

    Update keys indenting in config file

--- config.h
+++ config.h
@@ -24,33 +24,49 @@ static const char *colors[][3]      = {
 
 
 static Keys keys = {
-	&(Keys){NULL,	     
-		&(Keys){NULL,
-			&(Keys){NULL,
-				&(Keys){NULL,
-					&(Keys){NULL,
-						&(Keys){NULL,
-							&(Keys) {NULL,
-								 &(Keys){NULL,
-									 &(Keys){NULL,
-										 &(Keys){NULL,
-											 &(Keys){NULL,
-												 &(Keys){NULL,
-													 &(Keys){NULL,
-														 NULL,
-														 {LEADERMOD, XK_f, runorraise, {.v = browser}}}, /* C-t C-f */
-													 {LEADERMOD, XK_e, runorraise, {.v = emacs}}}, /* C-t C-e */
-												 {LEADERMOD, XK_t, selclient, {0}}}, /* C-t C-t */
-											 ShiftMask, XK_o, prevframe, {0}}, /* C-t O */
-										 {NOMODIFIER, XK_o, nextframe, {0}}}, /* C-t o */
-									 {NOMODIFIER, XK_p, prevclient, {0}}}, /* C-t p */
-								 {NOMODIFIER, XK_n, nextclient, {0}}}, /* C-t n */
-							{ShiftMask, XK_k, killclient, {0}}}, /* C-t K */
-						{NOMODIFIER, XK_q, quit, {0}}}, /* C-t q */
-					{LEADERMOD, XK_g, toggleleader, {.i = 0}}}, /* C-t C-g */
-				{NOMODIFIER, XK_b, banish, {0}}}, /* C-t b */
-		 {NOMODIFIER, XK_c, spawn, {.v = termcmd}}}, /* C-t c */
-		{NOMODIFIER, XK_exclam, spawn, {.v = dmenucmd}}}, /* C-t ! */
+	&(Keys)
+	{NULL,
+	 &(Keys)
+	 {NULL,
+	  &(Keys)
+	  {NULL,
+	   &(Keys)
+	   {NULL,
+	    &(Keys)
+	    {NULL,
+	     &(Keys)
+	     {NULL,
+	      &(Keys)
+	      {NULL,
+	       &(Keys)
+	       {NULL,
+		&(Keys)
+		{NULL,
+		 &(Keys)
+		 {NULL,
+		  &(Keys)
+		  {NULL,
+		   &(Keys)
+		   {NULL,
+		    &(Keys)
+		    {NULL,
+		     &(Keys)
+		     {NULL,
+		      NULL,
+		      {NOMODIFIER, XK_quotedbl, changeclient, {0}}}, /* C-t " */
+		     {LEADERMOD, XK_f, runorraise, {.v = browser}}}, /* C-t C-f */
+		    {LEADERMOD, XK_e, runorraise, {.v = emacs}}}, /* C-t C-e */
+		   {LEADERMOD, XK_t, selclient, {0}}}, /* C-t C-t */
+		  ShiftMask, XK_o, prevframe, {0}}, /* C-t O */
+		 {NOMODIFIER, XK_o, nextframe, {0}}}, /* C-t o */
+		{NOMODIFIER, XK_p, prevclient, {0}}}, /* C-t p */
+	       {NOMODIFIER, XK_n, nextclient, {0}}}, /* C-t n */
+	      {ShiftMask, XK_k, killclient, {0}}}, /* C-t K */
+	     {NOMODIFIER, XK_q, quit, {0}}}, /* C-t q */
+	    {LEADERMOD, XK_g, toggleleader, {.i = 0}}}, /* C-t C-g */
+	   {NOMODIFIER, XK_b, banish, {0}}}, /* C-t b */
+	  {NOMODIFIER, XK_c, spawn, {.v = termcmd}}}, /* C-t c */
+	 {NOMODIFIER, XK_exclam, spawn, {.v = dmenucmd}}}, /* C-t ! */
 	NULL,
 	{LEADERMOD, LEADERKEY, toggleleader, {.i = 1}}	    /* C-t */
 };
